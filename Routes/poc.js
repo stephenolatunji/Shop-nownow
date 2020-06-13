44f5599dd89728c38563cab46a68c40efe6850c6
Stephen Olatunji <SOlatunji01@gcn.ab-inbev.com>
2020-06-13 16:40:24 +0100

    Poc bug fix

--- Routes/poc.js
+++ Routes/poc.js
@@ -83,8 +83,8 @@ router.route('/:_id')
                 {_id: req.params._id},
                 {$set: {password: req.body.password}}
             )
-            const salt = await bcrypt.genSalt(10);
-            distributor.password = await bcrypt.hash(password, salt);
+            // const salt = await bcrypt.genSalt(10);
+            // poc.password = await bcrypt.hash(password, salt);
 
             res.status(200).json({
                 success: true,
